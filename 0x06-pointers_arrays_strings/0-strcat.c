96f8cd1ee3b5a6d0c79e8eb678656ad91e65b07b
Kehinde <kenlaw0120003@yahoo.com>
2023-03-02 07:27:35 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ * Return: void
+ */
+char *_strcat(char *dest, char *str)
+{
+
+int i = 0;
+int j = 0;
+
+i = 0;
+while (dest[i] != '\0')
+
+{
+
+i++;
+
+}
+
+j = 0;
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
+}
